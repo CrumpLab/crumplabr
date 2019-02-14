9b9893b4aea7765cf71826f28d203fc717d6583d
CrumpLab <crumpmj@gmail.com>
2019-02-14 08:09:40 -0500

    add pipe_print

--- R/count_words.r
+++ R/count_words.r
@@ -1,3 +1,8 @@
 count_words <- function(x){
-  return(length(unlist(strsplit(x,split=" "))))
+  if(length(x)==1){
+    return(length(unlist(strsplit(x,split=" "))))
+  } else if (length(x) > 1) {
+    return(unlist(lapply(x,
+                         FUN = function(y) return(length(unlist(strsplit(y,split=" ")))))))
+  }
 }
---
+++ R/pipe_print.r
@@ -0,0 +1,1 @@
+pipe_print <- function(data) {print(data); data}
